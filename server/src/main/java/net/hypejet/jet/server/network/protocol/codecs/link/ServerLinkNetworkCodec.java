2ecf809aeec5f6d1716eaf5054ae2ebbcd89853d
codestech <ryszardcichoszotawski@gmail.com>
2024-07-06 13:34:06 +0200

    feat: enum var-int codec

--- server/src/main/java/net/hypejet/jet/server/network/protocol/codecs/link/ServerLinkNetworkCodec.java
+++ server/src/main/java/net/hypejet/jet/server/network/protocol/codecs/link/ServerLinkNetworkCodec.java
@@ -8,6 +8,7 @@ import net.hypejet.jet.link.label.BuiltinLabel;
 import net.hypejet.jet.link.label.ComponentLabel;
 import net.hypejet.jet.link.label.ServerLinkLabel;
 import net.hypejet.jet.server.network.codec.NetworkCodec;
+import net.hypejet.jet.server.network.protocol.codecs.enums.EnumVarIntCodec;
 import net.hypejet.jet.server.util.NetworkUtil;
 import net.kyori.adventure.text.Component;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -26,22 +27,18 @@ public final class ServerLinkNetworkCodec implements NetworkCodec<ServerLink> {
 
     private static final ServerLinkNetworkCodec INSTANCE = new ServerLinkNetworkCodec();
 
-    private static final EnumMap<BuiltinLabel, Integer> builtInLabelToIdMap = new EnumMap<>(BuiltinLabel.class);
-    private static final IntObjectMap<BuiltinLabel> idToBuildInLabelMap = new IntObjectHashMap<>();
-
-    static {
-        builtInLabelToIdMap.put(BuiltinLabel.BUG_REPORT, 0);
-        builtInLabelToIdMap.put(BuiltinLabel.COMMUNITY_GUIDELINES, 1);
-        builtInLabelToIdMap.put(BuiltinLabel.SUPPORT, 2);
-        builtInLabelToIdMap.put(BuiltinLabel.STATUS, 3);
-        builtInLabelToIdMap.put(BuiltinLabel.FEEDBACK, 4);
-        builtInLabelToIdMap.put(BuiltinLabel.COMMUNITY, 5);
-        builtInLabelToIdMap.put(BuiltinLabel.WEBSITE, 6);
-        builtInLabelToIdMap.put(BuiltinLabel.FORUMS, 7);
-        builtInLabelToIdMap.put(BuiltinLabel.NEWS, 8);
-        builtInLabelToIdMap.put(BuiltinLabel.ANNOUNCEMENTS, 9);
-        builtInLabelToIdMap.forEach((label, id) -> idToBuildInLabelMap.put(id, label));
-    }
+    private static final EnumVarIntCodec<BuiltinLabel> builtInLabelCodec = EnumVarIntCodec.builder(BuiltinLabel.class)
+            .add(BuiltinLabel.BUG_REPORT, 0)
+            .add(BuiltinLabel.COMMUNITY_GUIDELINES, 1)
+            .add(BuiltinLabel.SUPPORT, 2)
+            .add(BuiltinLabel.STATUS, 3)
+            .add(BuiltinLabel.FEEDBACK, 4)
+            .add(BuiltinLabel.COMMUNITY, 5)
+            .add(BuiltinLabel.WEBSITE, 6)
+            .add(BuiltinLabel.FORUMS, 7)
+            .add(BuiltinLabel.NEWS, 8)
+            .add(BuiltinLabel.ANNOUNCEMENTS, 9)
+            .build();
 
     private ServerLinkNetworkCodec() {}
 
@@ -50,12 +47,7 @@ public final class ServerLinkNetworkCodec implements NetworkCodec<ServerLink> {
         ServerLinkLabel label;
 
         if (buf.readBoolean()) {
-            int buildInLabelId = NetworkUtil.readVarInt(buf);
-            label = idToBuildInLabelMap.get(buildInLabelId);
-
-            if (label == null) {
-                throw new IllegalArgumentException("Unknown build-in label identifier: " + buildInLabelId);
-            }
+            label = builtInLabelCodec.read(buf);
         } else {
             label = new ComponentLabel(NetworkUtil.readComponent(buf));
         }
@@ -69,13 +61,7 @@ public final class ServerLinkNetworkCodec implements NetworkCodec<ServerLink> {
         buf.writeBoolean(label instanceof BuiltinLabel);
 
         switch (label) {
-            case BuiltinLabel builtinLabel -> {
-                Integer builtInLabelId = builtInLabelToIdMap.get(builtinLabel);
-                if (builtInLabelId == null) {
-                    throw new IllegalArgumentException("Could not find an identifier for label: " + builtinLabel);
-                }
-                NetworkUtil.writeVarInt(buf, builtInLabelId);
-            }
+            case BuiltinLabel builtinLabel -> builtInLabelCodec.write(buf, builtinLabel);
             case ComponentLabel (Component component) -> NetworkUtil.writeComponent(buf, component);
         }
 
--- server/src/main/java/net/hypejet/jet/server/network/protocol/codecs/settings/PlayerSettingsCodec.java
+++ server/src/main/java/net/hypejet/jet/server/network/protocol/codecs/settings/PlayerSettingsCodec.java
@@ -2,8 +2,11 @@ package net.hypejet.jet.server.network.protocol.codecs.settings;
 
 import io.netty.buffer.ByteBuf;
 import net.hypejet.jet.entity.Entity;
+import net.hypejet.jet.entity.Entity.Hand;
 import net.hypejet.jet.entity.player.Player;
+import net.hypejet.jet.entity.player.Player.ChatMode;
 import net.hypejet.jet.server.network.codec.NetworkCodec;
+import net.hypejet.jet.server.network.protocol.codecs.enums.EnumVarIntCodec;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Collection;
@@ -22,6 +25,17 @@ public final class PlayerSettingsCodec implements NetworkCodec<Player.Settings> 
 
     private static final PlayerSettingsCodec INSTANCE = new PlayerSettingsCodec();
 
+    private static final EnumVarIntCodec<ChatMode> chatModeCodec = EnumVarIntCodec.builder(ChatMode.class)
+            .add(Player.ChatMode.ENABLED, 0)
+            .add(Player.ChatMode.COMMANDS_ONLY, 1)
+            .add(Player.ChatMode.HIDDEN, 2)
+            .build();
+
+    private static final EnumVarIntCodec<Hand> handCodec = EnumVarIntCodec.builder(Hand.class)
+            .add(Hand.LEFT, 0)
+            .add(Hand.RIGHT, 1)
+            .build();
+
     private PlayerSettingsCodec() {}
 
     @Override
@@ -29,12 +43,12 @@ public final class PlayerSettingsCodec implements NetworkCodec<Player.Settings> 
         Locale locale = LocaleNetworkCodec.instance().read(buf);
 
         byte viewDistance = buf.readByte();
-        Player.ChatMode chatMode = ChatModeNetworkCodec.instance().read(buf);
+        ChatMode chatMode = chatModeCodec.read(buf);
         boolean chatColorsEnabled = buf.readBoolean();
 
         Collection<Player.SkinPart> skinParts = SkinPartCollectionNetworkCodec.instance().read(buf);
 
-        Entity.Hand hand = HandNetworkCodec.instance().read(buf);
+        Hand hand = handCodec.read(buf);
         boolean enableTextFiltering = buf.readBoolean();
         boolean allowServerListings = buf.readBoolean();
 
@@ -46,10 +60,10 @@ public final class PlayerSettingsCodec implements NetworkCodec<Player.Settings> 
     public void write(@NonNull ByteBuf buf, Player.@NonNull Settings object) {
         LocaleNetworkCodec.instance().write(buf, object.locale());
         buf.writeByte(object.viewDistance());
-        ChatModeNetworkCodec.instance().write(buf, object.chatMode());
+        chatModeCodec.write(buf, object.chatMode());
         buf.writeBoolean(object.chatColorsEnabled());
         SkinPartCollectionNetworkCodec.instance().write(buf, object.enabledSkinParts());
-        HandNetworkCodec.instance().write(buf, object.mainHand());
+        handCodec.write(buf, object.mainHand());
         buf.writeBoolean(object.textFilteringEnabled());
         buf.writeBoolean(object.allowServerListings());
     }
