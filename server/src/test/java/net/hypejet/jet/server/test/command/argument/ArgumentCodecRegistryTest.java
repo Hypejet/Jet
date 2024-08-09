c4f4c77f9b7defdf297e9f176d0d7fd61b584639
codestech <ryszardcichoszotawski@gmail.com>
2024-08-09 15:03:33 +0200

    feat: argument codec tests

---
+++ server/src/test/java/net/hypejet/jet/server/test/command/argument/ArgumentCodecRegistryTest.java
@@ -0,0 +1,27 @@
+package net.hypejet.jet.server.test.command.argument;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.hypejet.jet.server.command.argument.ArgumentCodecRegistry;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Represents a test of a {@linkplain ArgumentCodecRegistry argument codec registry}.
+ *
+ * @since 1.0
+ * @author Codestech
+ * @see ArgumentCodecRegistry
+ */
+public final class ArgumentCodecRegistryTest {
+    @Test
+    public void testByClass() {
+        Assertions.assertNotNull(ArgumentCodecRegistry.codec(StringArgumentType.class));
+        Assertions.assertNull(ArgumentCodecRegistry.codec(String.class));
+    }
+
+    @Test
+    public void testById() {
+        Assertions.assertNotNull(ArgumentCodecRegistry.codec(0));
+        Assertions.assertNull(ArgumentCodecRegistry.codec(9));
+    }
+}
