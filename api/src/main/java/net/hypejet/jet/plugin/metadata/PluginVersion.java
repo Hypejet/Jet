ad6e267376be8f1cc111f5a1b04dfd8143749690
codestech <ryszardcichoszotawski@gmail.com>
2024-08-12 14:16:30 +0200

    chore: check if a plugin name in PluginVersion is null

--- api/src/main/java/net/hypejet/jet/plugin/metadata/PluginVersion.java
+++ api/src/main/java/net/hypejet/jet/plugin/metadata/PluginVersion.java
@@ -2,6 +2,8 @@ package net.hypejet.jet.plugin.metadata;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.Objects;
+
 /**
  * Represents a plugin version of a {@linkplain PluginDependency plugin dependency}.
  *
@@ -11,4 +13,16 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  * @since 1.0
  * @author Codestech
  */
-public record PluginVersion(@NonNull String name, boolean backwardsCompatible, boolean forwardCompatible) {}
+public record PluginVersion(@NonNull String name, boolean backwardsCompatible, boolean forwardCompatible) {
+    /**
+     * Constructs the {@linkplain PluginVersion plugin version}.
+     *
+     * @param name a name of the version
+     * @param backwardsCompatible whether all previous versions should also be supported
+     * @param forwardCompatible whether all newer versions should also be supported
+     * @since 1.0
+     */
+    public PluginVersion {
+        Objects.requireNonNull(name, "The name must not be null");
+    }
+}
