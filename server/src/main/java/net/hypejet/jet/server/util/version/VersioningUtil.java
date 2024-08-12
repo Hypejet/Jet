c2722d7f0ccfe1debc2cca7f9aa57611e6774683
codestech <ryszardcichoszotawski@gmail.com>
2024-08-12 15:08:56 +0200

    chore: convert alphabet chars to numbers while parsing versions

--- server/src/main/java/net/hypejet/jet/server/util/version/VersioningUtil.java
+++ server/src/main/java/net/hypejet/jet/server/util/version/VersioningUtil.java
@@ -3,7 +3,6 @@ package net.hypejet.jet.server.util.version;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Represents a utility for parsing version name strings.
@@ -14,6 +13,9 @@ import java.util.stream.Collectors;
 public final class VersioningUtil {
 
     private static final String VERSION_SPLITTER = "\\.";
+
+    private static final int ALPHABET_START = 65;
+    private static final int ALPHABET_END = 90;
 
     private VersioningUtil() {}
 
@@ -31,14 +33,23 @@ public final class VersioningUtil {
         int[] parsed = new int[split.length];
 
         for (int index = 0; index < split.length; index++) {
-            String filteredPart = split[index].chars()
-                    .filter(Character::isDigit)
-                    .mapToObj(Character::toString)
-                    .collect(Collectors.joining());
+            StringBuilder builder = new StringBuilder();
+            char[] characters = split[index].toCharArray();
+
+            for (char character : characters) {
+                if (Character.isDigit(character)) {
+                    builder.append(character);
+                    continue;
+                }
+
+                character = Character.toUpperCase(character);
+                if (character >= ALPHABET_START && character <= ALPHABET_END)
+                    builder.append(character - ALPHABET_START);
+            }
 
             int parsedPart = 0;
-            if (!filteredPart.isEmpty())
-                parsedPart = Integer.parseInt(filteredPart);
+            if (!builder.isEmpty())
+                parsedPart = Integer.parseInt(builder.toString());
 
             parsed[index] = parsedPart;
         }
