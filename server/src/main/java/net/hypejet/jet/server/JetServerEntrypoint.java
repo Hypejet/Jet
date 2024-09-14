ff81d9272ee663bdf7bd9934af84c541fba19815
codestech1 <ryszardcichoszotawski@gmail.com>
2024-09-14 10:37:05 +0200

    chore: cleanup

--- server/src/main/java/net/hypejet/jet/server/JetServerEntrypoint.java
+++ server/src/main/java/net/hypejet/jet/server/JetServerEntrypoint.java
@@ -1,9 +1,5 @@
 package net.hypejet.jet.server;
 
-import net.hypejet.jet.event.events.packet.PacketSendEvent;
-import net.hypejet.jet.protocol.packet.server.configuration.ServerRegistryDataConfigurationPacket;
-import net.kyori.adventure.key.Key;
-
 /**
  * Represents a main class, which provides an instruction for when the program starts executing.
  *
@@ -22,11 +18,6 @@ public final class JetServerEntrypoint {
      */
     public static void main(String[] args) {
         JetMinecraftServer server = new JetMinecraftServer();
-        server.eventNode().addListener(event -> {
-            if (event.getPacket() instanceof ServerRegistryDataConfigurationPacket packet)
-                if (packet.registry().equals(Key.key("worldgen/biome")))
-                    System.out.println(packet);
-        }, PacketSendEvent.class);
         Runtime.getRuntime().addShutdownHook(Thread.ofVirtual().unstarted(server::shutdown));
     }
 }
