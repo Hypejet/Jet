c2925d16116e8f2575bbf5668da814fd8f128d65
codestech <ryszardcichoszotawski@gmail.com>
2024-12-31 22:24:16 +0100

    chore: cleanup event tests

--- api/src/test/java/net/hypejet/jet/event/EventListenerTest.java
+++ api/src/test/java/net/hypejet/jet/event/EventListenerTest.java
@@ -1,5 +1,6 @@
 package net.hypejet.jet.event;
 
+import net.hypejet.jet.event.listener.EventListener;
 import net.hypejet.jet.event.priority.EventPriority;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,10 +8,9 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * Represents a test for {@linkplain EventListener event listener}.
+ * Represents a test of {@linkplain EventListener an event listener}.
  *
  * @since 1.0
- * @author Codestech
  */
 public final class EventListenerTest {
     @Test
@@ -18,7 +18,7 @@ public final class EventListenerTest {
         Class<?> eventClass = Integer.class;
         EventPriority priority = EventPriority.EARLY;
 
-        EventListener<?> listener = EventListener.listener(event -> {}, eventClass, priority);
+        EventListener<?> listener = new EventListener<>(event -> {}, eventClass, priority);
 
         Assertions.assertEquals(eventClass, listener.eventClass());
         Assertions.assertEquals(priority, listener.priority());
@@ -27,7 +27,7 @@ public final class EventListenerTest {
     @Test
     public void testListenerCalling() {
         AtomicBoolean called = new AtomicBoolean();
-        EventListener<String> listener = EventListener.listener(event -> called.set(true), String.class);
+        EventListener<String> listener = new EventListener<>(event -> called.set(true), String.class);
 
         Assertions.assertFalse(called.get());
         listener.call("Hello World, Hello Jet users!");
@@ -36,7 +36,7 @@ public final class EventListenerTest {
 
     @Test
     public void testListenerEligible() {
-        EventListener<String> listener = EventListener.listener(event -> {}, String.class, event -> event.equals("A"));
+        EventListener<String> listener = new EventListener<>(event -> {}, String.class, event -> event.equals("A"));
         Assertions.assertFalse(listener.isEligible("a"));
         Assertions.assertTrue(listener.isEligible("A"));
         Assertions.assertFalse(listener.isEligible("B"));
--- api/src/test/java/net/hypejet/jet/event/EventNodeTest.java
+++ api/src/test/java/net/hypejet/jet/event/EventNodeTest.java
@@ -1,58 +1,66 @@
 package net.hypejet.jet.event;
 
+import net.hypejet.concurrency.collection.CollectionAcquisition;
 import net.hypejet.jet.event.annotation.Subscribe;
+import net.hypejet.jet.event.listener.EventListener;
+import net.hypejet.jet.event.node.EventNode;
 import net.hypejet.jet.event.priority.EventPriority;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * Represents a test for {@linkplain IEventNode event node}.
+ * Represents a test of {@linkplain EventNode an event node}.
  *
  * @since 1.0
- * @author Codestech
  */
 public final class EventNodeTest {
     @Test
     public void testChildren() {
-        IEventNode<Object> node = IEventNode.create(Object.class);
-        Assertions.assertTrue(node.children().isEmpty());
+        EventNode<Object> node = new EventNode<>(Object.class);
+        try (CollectionAcquisition<EventNode<?>, ?> acquisition = node.children()) {
+            Collection<EventNode<?>> collection = acquisition.collection();
+            Assertions.assertTrue(collection.isEmpty());
 
-        List<IEventNode<Object>> children = List.of(IEventNode.create(Object.class), IEventNode.create(Object.class));
+            List<EventNode<Object>> children = List.of(new EventNode<>(Object.class), new EventNode<>(Object.class));
 
-        children.forEach(node::addChild);
-        Assertions.assertTrue(node.children().containsAll(children));
+            children.forEach(node::addChild);
+            Assertions.assertTrue(collection.containsAll(children));
 
-        children.forEach(node::removeChild);
-        Assertions.assertTrue(node.children().isEmpty());
+            children.forEach(node::removeChild);
+            Assertions.assertTrue(collection.isEmpty());
+        }
     }
 
     @Test
     public void testListeners() {
-        IEventNode<Object> node = IEventNode.create(Object.class);
-        Assertions.assertTrue(node.listeners().isEmpty());
+        EventNode<Object> node = new EventNode<>(Object.class);
+        try (CollectionAcquisition<EventListener<?>, ?> acquisition = node.listeners()) {
+            Collection<EventListener<?>> collection = acquisition.collection();
+            Assertions.assertTrue(collection.isEmpty());
 
-        List<EventListener<Object>> listeners = new ArrayList<>();
+            List<EventListener<Object>> listeners = new ArrayList<>();
 
-        listeners.add(EventListener.listener(event -> {}, Object.class));
-        listeners.add(EventListener.listener(event -> {}, String.class));
-        listeners.forEach(node::addListener);
+            listeners.add(new EventListener<>(event -> {}, Object.class));
+            listeners.add(new EventListener<>(event -> {}, String.class));
+            listeners.forEach(node::addListener);
 
-        listeners.add(node.addListener(event -> {}, Integer.class));
+            listeners.add(node.addListener(event -> {}, Integer.class));
+            Assertions.assertTrue(collection.containsAll(listeners));
 
-        Assertions.assertTrue(node.listeners().containsAll(listeners));
-
-        listeners.forEach(node::removeListener);
-        Assertions.assertTrue(node.listeners().isEmpty());
+            listeners.forEach(node::removeListener);
+            Assertions.assertTrue(collection.isEmpty());
+        }
     }
 
     @Test
     public void testCalling() {
-        IEventNode<Object> node = IEventNode.create(Object.class);
+        EventNode<Object> node = new EventNode<>(Object.class);
 
         CountDownLatch latch = new CountDownLatch(4);
         TestClassListener listener = new TestClassListener(latch);
@@ -73,7 +81,7 @@ public final class EventNodeTest {
         EventPriority priority = EventPriority.LATE;
         Class<?> eventClass = Integer.class;
 
-        IEventNode<?> node = IEventNode.create(eventClass, priority);
+        EventNode<?> node = new EventNode<>(eventClass, priority);
 
         Assertions.assertEquals(priority, node.priority());
         Assertions.assertEquals(eventClass, node.eventClass());
@@ -84,7 +92,6 @@ public final class EventNodeTest {
         public void call(@NonNull Object object) {
             this.latch.countDown();
         }
-
 
         @Subscribe
         public void call2(@NonNull Object object) {
