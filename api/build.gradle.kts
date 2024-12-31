4a144188d80bf5e0c4e14896271c8a04072b02a7
codestech <ryszardcichoszotawski@gmail.com>
2024-12-31 22:30:40 +0100

    chore: include MIT license into API's pom

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -18,6 +18,12 @@ dependencies {
 publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
+        pom.licenses {
+            license {
+                name = "MIT"
+                url = "https://choosealicense.com/licenses/mit/"
+            }
+        }
     }
 }
 
