439ee86326d75fea847befd2a597f9c55631941c
codestech <ryszardcichoszotawski@gmail.com>
2025-07-28 12:46:56 +0200

    feat: items

---
+++ api/src/main/java/net/hypejet/jet/inventory/item/Item.java
@@ -0,0 +1,11 @@
+package net.hypejet.jet.inventory.item;
+
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * A Minecraft item.
+ *
+ * @since 1.0
+ */
+@ApiStatus.NonExtendable
+public interface Item {}
