68e95f526ecb92e298fd7c585b662dad521e031f
codestech <ryszardcichoszotawski@gmail.com>
2025-07-28 13:26:26 +0200

    feat: cow variants

--- api/src/main/java/net/hypejet/jet/entity/variant/cow/CowVariant.java
+++ api/src/main/java/net/hypejet/jet/entity/variant/cow/CowVariant.java
@@ -1,4 +1,4 @@
-package net.hypejet.jet.entity.variant.pig;
+package net.hypejet.jet.entity.variant.cow;
 
 import net.kyori.adventure.key.Key;
 import org.jspecify.annotations.NonNull;
@@ -6,33 +6,33 @@ import org.jspecify.annotations.NonNull;
 import java.util.Objects;
 
 /**
- * A variant of Minecraft pig entity.
+ * A variant of Minecraft cow entity.
  *
- * @param modelType the pig model type to use for entities of this variant
- * @param asset the key of the texture that pigs of this variant should have
+ * @param modelType the cow model type to use for entities of this variant
+ * @param asset the key of the texture that cows of this variant should have
  * @since 1.0
  */
-public record PigVariant(@NonNull ModelType modelType, @NonNull Key asset) {
+public record CowVariant(@NonNull ModelType modelType, @NonNull Key asset) {
     /**
-     * Constructs the {@linkplain PigVariant pig variant}.
+     * Constructs the {@linkplain CowVariant cow variant}.
      *
-     * @param modelType the pig model type to use for entities of this variant
-     * @param asset the key of the texture that pigs of this variant should have
+     * @param modelType the cow model type to use for entities of this variant
+     * @param asset the key of the texture that cows of this variant should have
      * @since 1.0
      */
-    public PigVariant {
+    public CowVariant {
         Objects.requireNonNull(modelType, "model type");
         Objects.requireNonNull(asset, "asset");
     }
 
     /**
-     * The model type of {@linkplain PigVariant pig variants}.
+     * The model type of {@linkplain CowVariant cow variants}.
      *
      * <p>This is not an enum since it depends on Minecraft.
      * Adding new entries could break switch cases for example.</p>
      *
      * @since 1.0
-     * @see PigVariant
+     * @see CowVariant
      */
     public static final class ModelType {
         /**
@@ -43,11 +43,18 @@ public record PigVariant(@NonNull ModelType modelType, @NonNull Key asset) {
         public static final ModelType NORMAL = new ModelType("normal");
 
         /**
-         * A model type used by cold pig variants.
+         * A model type used by cold cow variants.
          *
          * @since 1.0
          */
         public static final ModelType COLD = new ModelType("cold");
+
+        /**
+         * A model type used by warm cow variants.
+         *
+         * @since 1.0
+         */
+        public static final ModelType WARM = new ModelType("warm");
 
         private final String name;
 
