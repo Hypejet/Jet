9f2eca584fa22b6a18b9e16c018c409467695730
codestech <ryszardcichoszotawski@gmail.com>
2025-07-28 07:58:03 +0200

    feat: sound events

---
+++ api/src/main/java/net/hypejet/jet/world/sound/SoundEvent.java
@@ -0,0 +1,27 @@
+package net.hypejet.jet.world.sound;
+
+import net.kyori.adventure.key.Key;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Objects;
+
+/**
+ * A definition of a Minecraft sound related to a specific event.
+ *
+ * @param sound a key of the sound
+ * @param range the fixed range of the sound, {@code null} if the sound should be played with a variable range
+ * @since 1.0
+ */
+public record SoundEvent(@NonNull Key sound, @Nullable Float range) {
+    /**
+     * Constructs the {@linkplain SoundEvent sound event}.
+     *
+     * @param sound a key of the sound
+     * @param range the fixed range of the sound, {@code null} if the sound should be played with a variable range
+     * @since 1.0
+     */
+    public SoundEvent {
+        Objects.requireNonNull(sound, "sound");
+    }
+}
