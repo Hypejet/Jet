1d18800848fa8c4b3055a01cf661bc4186acbc32
codestech <ryszardcichoszotawski@gmail.com>
2025-07-29 08:49:56 +0200

    feat: dimension types

---
+++ api/src/main/java/net/hypejet/jet/world/dimension/DimensionType.java
@@ -0,0 +1,113 @@
+package net.hypejet.jet.world.dimension;
+
+import net.hypejet.jet.util.game.number.IntProvider;
+import net.kyori.adventure.key.Key;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Objects;
+
+/**
+ * A type of Minecraft dimension.
+ *
+ * @param fixedTime the fixed daytime that worlds using this dimension type should have, {@code null} if these worlds
+ *                  should have a normal day cycle
+ * @param hasSkyLight whether worlds using this dimension type should have skylight
+ * @param hasCeiling whether worlds using this dimension type have a bedrock ceiling
+ * @param ultraWarm whether world using this dimension type should have nether-like water behaviour
+ * @param natural when {@code true}, nether portals in worlds using this dimension type spawn zombified piglins
+ *                and creaking hearts can spawn creakings, when {@code false}, compasses spin randomly and using
+ *                a bed to set respawn point or sleep is disabled
+ * @param coordinateScale the multiplier that should be applied to coordinates of players leaving worlds
+ *                        using this dimension type
+ * @param bedWorks when {@code false}, using the bed in worlds using this dimension type makes the used bed blow up
+ * @param respawnAnchorWorks when {@code false}, using the respawn anchor in worlds using this dimension type
+ *                           makes the used respawn anchor blow up
+ * @param minY the minimum height in which blocks can exist within worlds using this dimension type
+ * @param height the total height in which blocks can exist within worlds using this dimension type
+ * @param logicalHeight the maximum height to which chorus fruits and nether portals can bring players within
+ *                    worlds using this dimension type
+ * @param infiniburn the key of block tag which should make fire on blocks using that tag burn infinitely
+ * @param effects the key of the dimension effect that worlds using this dimension type should use
+ * @param ambientLight how much light worlds using this dimension should have, where {@code 0} makes it completely
+ *                     follow the light level and {@code 1} makes it disable the ambient lighting
+ * @param cloudHeight determines the lower edge of the clouds that worlds using this dimension type should have,
+ *                    {@code null} if clouds should be disabled in these worlds
+ * @param monsterSettings behaviour settings that monsters in worlds using this dimension type should have
+ * @since 1.0
+ */
+// TODO: Add range checks?
+public record DimensionType(@Nullable Long fixedTime, boolean hasSkyLight, boolean hasCeiling,
+                            boolean ultraWarm, boolean natural, double coordinateScale,
+                            boolean bedWorks, boolean respawnAnchorWorks, int minY, int height,
+                            int logicalHeight, @NonNull Key infiniburn, @NonNull Key effects,
+                            float ambientLight, @Nullable Integer cloudHeight,
+                            @NonNull MonsterSettings monsterSettings) {
+    /**
+     * Constructs the {@linkplain DimensionType dimension type}.
+     *
+     * @param fixedTime the fixed daytime that worlds using this dimension type should have, {@code null} if these
+     *                  worlds should have a normal day cycle
+     * @param hasSkyLight whether worlds using this dimension type should have skylight
+     * @param hasCeiling whether worlds using this dimension type have a bedrock ceiling
+     * @param ultraWarm whether world using this dimension type should have nether-like water behaviour
+     * @param natural when {@code true}, nether portals in worlds using this dimension type spawn zombified piglins
+     *                and creaking hearts can spawn creakings, when {@code false}, compasses spin randomly and using
+     *                a bed to set respawn point or sleep is disabled
+     * @param coordinateScale the multiplier that should be applied to coordinates of players leaving worlds
+     *                        using this dimension type
+     * @param bedWorks when {@code false}, using the bed in worlds using this dimension type makes the used bed blow up
+     * @param respawnAnchorWorks when {@code false}, using the respawn anchor in worlds using this dimension type
+     *                           makes the used respawn anchor blow up
+     * @param minY the minimum height in which blocks can exist within worlds using this dimension type
+     * @param height the total height in which blocks can exist within worlds using this dimension type
+     * @param logicalHeight the maximum height to which chorus fruits and nether portals can bring players within
+     *                      worlds using this dimension type
+     * @param infiniburn the key of block tag which should make fire on blocks using that tag burn infinitely
+     * @param effects the key of the dimension effect that worlds using this dimension type should use
+     * @param ambientLight how much light worlds using this dimension should have, where {@code 0} makes it completely
+     *                     follow the light level and {@code 1} makes it disable the ambient lighting
+     * @param cloudHeight determines the lower edge of the clouds that worlds using this dimension type should have,
+     *                    {@code null} if clouds should be disabled in these worlds
+     * @param monsterSettings behaviour settings that monsters in worlds using this dimension type should have
+     * @since 1.0
+     */
+    public DimensionType {
+        Objects.requireNonNull(infiniburn, "infiniburn");
+        Objects.requireNonNull(effects, "effects");
+        Objects.requireNonNull(monsterSettings, "monster settings");
+    }
+
+    /**
+     * Monster settings of a {@linkplain DimensionType dimension type}.
+     *
+     * @param piglinSafe when {@code false}, piglins and hoglins in worlds using the dimension type associated
+     *                   with these monster settings transform to zombified entities
+     * @param hasRaids whether players with the bad omen effect can cause a raid in world using the dimension type
+     *                 associated with these monster settings
+     * @param spawnLightTest the maximum light level value required to make monsters spawn in the dimension type
+     *                       associated with these monster settings
+     * @param spawnBlockLightLimit the maximum block light level value required to make monsters spawn in the dimension
+     *                             type associated with these monster settings
+     * @since 1.0
+     */
+    public record MonsterSettings(boolean piglinSafe, boolean hasRaids, @NonNull IntProvider spawnLightTest,
+                                  int spawnBlockLightLimit) {
+        /**
+         * Constructs the {@linkplain MonsterSettings monster settings}.
+         *
+         * @param piglinSafe when {@code false}, piglins and hoglins in worlds using the dimension type associated
+         *                   with these monster settings transform to zombified entities
+         * @param hasRaids whether players with the bad omen effect can cause a raid in world using the dimension type
+         *                 associated with these monster settings
+         * @param spawnLightTest the maximum light level value required to make monsters spawn in the dimension type
+         *                       associated with these monster settings
+         * @param spawnBlockLightLimit the maximum block light level value required to make monsters spawn
+         *                             in the dimension type associated with these monster settings
+         * @since 1.0
+         */
+        public MonsterSettings {
+            Objects.requireNonNull(spawnLightTest, "spawn light test");
+        }
+    }
+}
