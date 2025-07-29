242fab5c2499c1e90c38960825711e10c3c8e681
codestech <ryszardcichoszotawski@gmail.com>
2025-07-29 14:45:56 +0200

    feat: binary tag utilities

---
+++ server/src/main/java/net/hypejet/jet/server/util/nbt/BinaryTagUtil.java
@@ -0,0 +1,94 @@
+package net.hypejet.jet.server.util.nbt;
+
+import net.kyori.adventure.nbt.BinaryTag;
+import net.kyori.adventure.nbt.BinaryTagType;
+import net.kyori.adventure.nbt.ByteBinaryTag;
+import net.kyori.adventure.nbt.CompoundBinaryTag;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+/**
+ * Utilities for {@linkplain BinaryTag binary-tag} management.
+ *
+ * @since 1.0
+ * @see BinaryTag
+ */
+public final class BinaryTagUtil {
+
+    private BinaryTagUtil() {}
+
+    /**
+     * Gets a required tag from the specified {@linkplain CompoundBinaryTag compound binary tag}.
+     *
+     * @param name a name of the tag to get
+     * @param compound the compound binary tag
+     * @return the tag
+     * @throws IllegalArgumentException if the tag has not been specified
+     * @since 1.0
+     */
+    public static @NonNull BinaryTag requiredTag(@NonNull String name, @NonNull CompoundBinaryTag compound) {
+        BinaryTag tag = compound.get(name);
+        if (tag == null)
+            throw new IllegalArgumentException(String.format("Tag \"%s\" has not been specified", name));
+        return tag;
+    }
+
+    /**
+     * Gets a required tag from the specified {@linkplain CompoundBinaryTag compound binary tag}.
+     *
+     * @param name a name of the tag to get
+     * @param compound the compound binary tag
+     * @param tagType a type of the tag to get
+     * @return the tag
+     * @param <B> the type of the tag
+     * @throws IllegalArgumentException if the tag is not of the specified type or the tag has not been specified
+     * @since 1.0
+     */
+    public static <B extends BinaryTag> @NonNull B requiredTag(@NonNull String name,
+                                                               @NonNull CompoundBinaryTag compound,
+                                                               @NonNull BinaryTagType<B> tagType) {
+        B tag = optionalTag(name, compound, tagType);
+        if (tag == null)
+            throw new IllegalArgumentException(String.format("Tag \"%s\" has not been specified", name));
+        return tag;
+    }
+
+    /**
+     * Gets an optional tag from the specified {@linkplain CompoundBinaryTag compound binary tag}.
+     *
+     * @param name a name of the tag to get
+     * @param compound the compound binary tag
+     * @param tagType a type of the tag to get
+     * @return the tag, {@code null} if the tag has not been specified
+     * @param <B> the type of the tag
+     * @throws IllegalArgumentException if the tag is not of the specified type
+     * @since 1.0
+     */
+    public static <B extends BinaryTag> @Nullable B optionalTag(@NonNull String name,
+                                                                @NonNull CompoundBinaryTag compound,
+                                                                @NonNull BinaryTagType<B> tagType) {
+        BinaryTag tag = compound.get(name);
+        if (tag == null) {
+            return null;
+        } else if (tag.type() == tagType) {
+            // noinspection unchecked ; we checked whether the tag type is the specified type
+            return (B) tag;
+        } else {
+            throw new IllegalArgumentException(String.format(
+                    "Tag \"%s\" must be of %s type",
+                    name, tagType.getClass().getSimpleName()
+            ));
+        }
+    }
+
+    /**
+     * Gets a {@code boolean} representation of the specified {@linkplain ByteBinaryTag byte binary tag}.
+     *
+     * @param tag the byte binary tag to represent as a boolean
+     * @return the boolean representation of the specified tag
+     * @since 1.0
+     */
+    public static boolean booleanValue(@NonNull ByteBinaryTag tag) {
+        return tag.value() != 0; // != might seem weird, but that is what Minecraft does
+    }
+}
