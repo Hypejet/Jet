4784f890d5eca3a6dc7a1e5ed09c1cf7c22ee7b5
codestech <ryszardcichoszotawski@gmail.com>
2025-07-30 13:42:18 +0200

    feat: wolf variant serialization ; separate asset info of WolfVariant from the main class to a nested class

--- api/src/main/java/net/hypejet/jet/entity/variant/wolf/WolfVariant.java
+++ api/src/main/java/net/hypejet/jet/entity/variant/wolf/WolfVariant.java
@@ -8,23 +8,41 @@ import java.util.Objects;
 /**
  * A variant of Minecraft wolf entity.
  *
- * @param wildAsset the key of the texture to use when the wolf with of variant is wild
- * @param tameAsset the key of the texture to use when the wolf with of variant is tamed
- * @param angryAsset the key of the texture to use when the wolf with of variant is angry
+ * @param assetInfo a set of textures that entities of this variant should use
  * @since 1.0
  */
-public record WolfVariant(@NonNull Key wildAsset, @NonNull Key tameAsset, @NonNull Key angryAsset) {
+public record WolfVariant(@NonNull AssetInfo assetInfo) {
     /**
      * Constructs the {@linkplain WolfVariant wolf variant}.
      *
-     * @param wildAsset the key of the texture to use when the wolf with of variant is wild
-     * @param tameAsset the key of the texture to use when the wolf with of variant is tamed
-     * @param angryAsset the key of the texture to use when the wolf with of variant is angry
+     * @param assetInfo a set of textures that entities of this variant should use
      * @since 1.0
      */
     public WolfVariant {
-        Objects.requireNonNull(wildAsset, "wild asset");
-        Objects.requireNonNull(tameAsset, "tame asset");
-        Objects.requireNonNull(angryAsset, "angry asset");
+        Objects.requireNonNull(assetInfo, "asset info");
+    }
+
+    /**
+     * A set of textures to use for a wolf entity.
+     *
+     * @param wildAsset the key of the texture to use when the wolf is wild
+     * @param tameAsset the key of the texture to use when the wolf is tamed
+     * @param angryAsset the key of the texture to use when the wolf is angry
+     * @since 1.0
+     */
+    public record AssetInfo(@NonNull Key wildAsset, @NonNull Key tameAsset, @NonNull Key angryAsset) {
+        /**
+         * Constructs the {@linkplain AssetInfo asset info}.
+         *
+         * @param wildAsset the key of the texture to use when the wolf is wild
+         * @param tameAsset the key of the texture to use when the wolf is tamed
+         * @param angryAsset the key of the texture to use when the wolf is angry
+         * @since 1.0
+         */
+        public AssetInfo {
+            Objects.requireNonNull(wildAsset, "wild asset");
+            Objects.requireNonNull(tameAsset, "tame asset");
+            Objects.requireNonNull(angryAsset, "angry asset");
+        }
     }
 }
