6716b69e0e229249ba2ba3bda1b6e2c0e94b238a
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2025-08-27 11:08:53 +0300

    docs(network): add missing Javadoc for ServerEntityAnimationPlayPacketWriter

--- server/src/main/java/net/hypejet/jet/server/network/codec/packet/server/play/ServerEntityAnimationPlayPacketWriter.java
+++ server/src/main/java/net/hypejet/jet/server/network/codec/packet/server/play/ServerEntityAnimationPlayPacketWriter.java
@@ -7,9 +7,24 @@ import net.hypejet.jet.server.network.codec.NetworkWriter;
 import net.hypejet.jet.server.network.codec.number.VarIntNetworkCodec;
 import net.hypejet.jet.server.network.packet.packets.server.play.ServerEntityAnimationPlayPacket;
 
+/**
+ * Represents {@linkplain NetworkWriter a network writer}, which writes
+ * {@linkplain ServerEntityAnimationPlayPacket a server entity animation play packet}.
+ *
+ * @since 1.0
+ * @see ServerEntityAnimationPlayPacket
+ * @see NetworkWriter
+ */
 public final class ServerEntityAnimationPlayPacketWriter implements NetworkWriter<ServerEntityAnimationPlayPacket> {
     
+    /**
+     * An instance of the {@linkplain ServerEntityAnimationPlayPacketWriter server entity animation play packet writer}.
+     *
+     * @since 1.0
+     */
     public static final ServerEntityAnimationPlayPacketWriter INSTANCE = new ServerEntityAnimationPlayPacketWriter();
+
+    private ServerEntityAnimationPlayPacketWriter() {}
 
     @Override
     public void write(@NonNull ByteBuf buf, @NonNull ServerEntityAnimationPlayPacket object) {
